7ed37bb62f1cbbf9135063c616aeedb539a90747
jared <114547220+goldjared@users.noreply.github.com>
2023-02-07 13:11:59 -0500

    Create object to hold book info

---
+++ bookobj.js
@@ -0,0 +1,9 @@
+function book(name, author, pages, read) {
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.read = read
+};
+
+const book1 = new book('The Hobbit', 'J.R.R Tolkien', '333', 'In process of reading');
+
