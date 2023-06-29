372575ed3b8aea7ceab5122b2cb264000d5fe5e4
jared <jaredgold@proton.me>
2023-06-29 18:10:53 -0400

    test ceasarCypher func

--- jest-practice/index.test.js
+++ jest-practice/index.test.js
@@ -1,26 +1,29 @@
-import { capitalize, reverseString, calculator } from './index';
+import { capitalize, reverseString, calculator,ceasarCypher } from './index';
 
-test('first letter should be capital', () => {
-  expect(capitalize('test')).toBe('Test');
-});
+// test('first letter should be capital', () => {
+//   expect(capitalize('test')).toBe('Test');
+// });
 
-test('string should be reversed', () => {
-  expect(reverseString('tester')).toBe('retset');
-});
+// test('string should be reversed', () => {
+//   expect(reverseString('tester')).toBe('retset');
+// });
 
-test('calc obj should add', () => {
-  expect(calculator.add(2,2)).toBe(4)
+// test('calc obj should add', () => {
+//   expect(calculator.add(2,2)).toBe(4)
 
-});
+// });
 
-test('calc obj should subtract', () => {
-  expect(calculator.subtract(5,2)).toBe(3)
-  });
+// test('calc obj should subtract', () => {
+//   expect(calculator.subtract(5,2)).toBe(3)
+//   });
 
-test('calc obj should mult', () => {
-  expect(calculator.multiply(100, 4)).toBe(400)
-});
+// test('calc obj should mult', () => {
+//   expect(calculator.multiply(100, 4)).toBe(400)
+// });
 
-test('calc obj should divide', () => {
- expect(calculator.divide(10,2)).toBe(5)
+// test('calc obj should divide', () => {
+//  expect(calculator.divide(10,2)).toBe(5)
+// });
+test('ceasar cypher with shift of 1', () => {
+  expect(ceasarCypher('kitten', 1)).toBe('ljuufo')
 });
