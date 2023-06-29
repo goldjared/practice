bfafa2f54ca983b0a33c333e4135db68902c34fb
jared <jaredgold@proton.me>
2023-06-29 14:20:53 -0400

    create test reversestring

--- jest-practice/index.test.js
+++ jest-practice/index.test.js
@@ -1,5 +1,9 @@
-import capitalize from './index';
+import { capitalize, reverseString } from './index';
 
 test('first letter should be capital', () => {
   expect(capitalize('test')).toBe('Test');
+})
+
+test('string should be reversed', () => {
+  expect(reverseString('tester')).toBe('retset');
 })
