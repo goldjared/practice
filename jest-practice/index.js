cc5fd54c8591ec07558966a3f349f1747f4e6fab
jared <jaredgold@proton.me>
2023-06-29 18:10:35 -0400

    create ceasarCypher function

--- jest-practice/index.js
+++ jest-practice/index.js
@@ -17,4 +17,16 @@ let calculator = {
   divide: (x,y) => x / y
 }
 
-module.exports = { capitalize, reverseString, calculator } 
+function ceasarCypher(text, keyVal) {
+  let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  let textArray = text.split('')
+  let cypheredWord = [];
+  textArray.forEach(letter => {
+    cypheredWord.push(alphabet[alphabet.indexOf(letter) + keyVal]);
+  })
+  return cypheredWord.join('');
+}
+
+console.log(ceasarCypher('kitten', 1));
+
+module.exports = { capitalize, reverseString, calculator, ceasarCypher } 
