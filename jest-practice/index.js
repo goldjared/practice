e45eee12e505d4e4b872361b3e7c566d51904eb1
jared <114547220+goldjared@users.noreply.github.com>
2023-06-29 19:37:07 -0400

    implement ceasarcipher func, and helper func to lowercase string

--- jest-practice/index.js
+++ jest-practice/index.js
@@ -17,16 +17,25 @@ let calculator = {
   divide: (x,y) => x / y
 }
 
-function ceasarCypher(text, keyVal) {
+function preProcessCeasarCipher(string) {
+  string = string.toLowerCase();
+  return string;
+
+}
+
+function ceasarCipher(text, keyVal) {
   let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  text = preProcessCeasarCipher(text)
+  console.log(text, 'here');
   let textArray = text.split('')
   let cypheredWord = [];
   textArray.forEach(letter => {
+    if(letter === 'z') return cypheredWord.push('a')
     cypheredWord.push(alphabet[alphabet.indexOf(letter) + keyVal]);
   })
   return cypheredWord.join('');
 }
 
-console.log(ceasarCypher('kitten', 1));
+console.log(ceasarCipher('kitten', 1));
 
-module.exports = { capitalize, reverseString, calculator, ceasarCypher } 
+module.exports = { capitalize, reverseString, calculator, ceasarCipher } 
