d451a602a93e00e251e0b9e2c35db60d6ee3b2d1
jared <jaredgold@proton.me>
2023-06-29 19:37:22 -0400

    comment previous tests

--- jest-practice/index.test.js
+++ jest-practice/index.test.js
@@ -24,16 +24,16 @@ import { capitalize, reverseString, calculator, ceasarCipher } from './index';
 // test('calc obj should divide', () => {
 //  expect(calculator.divide(10,2)).toBe(5)
 // });
-// test('ceasar cypher with shift of 1', () => {
-//   expect(ceasarCipher('kitten', 1)).toBe('ljuufo')
-// });
-// test('ceasar cypher with shift of 1 and z', () => {
-//   expect(ceasarCipher('kittenz', 1)).toBe('ljuufoa')
-// });
+  // test('ceasar cypher with shift of 1', () => {
+  //   expect(ceasarCipher('kitten', 1)).toBe('ljuufo')
+  // });
+  // test('ceasar cypher with shift of 1 and z', () => {
+  //   expect(ceasarCipher('kittenz', 1)).toBe('ljuufoa')
+  // });
 
-// test('ceasar cypher with shift of 1 and z in middle', () => {
-//   expect(ceasarCipher('heyzitten', 1)).toBe('ifzajuufo')
-// });
-// test('ceasar cypher shift 1, test puncuation', () => {
-//   expect(ceasarCipher('KITTEN', 1)).toBe('ljuufo')
-// });
+  // test('ceasar cypher with shift of 1 and z in middle', () => {
+  //   expect(ceasarCipher('heyzitten', 1)).toBe('ifzajuufo')
+  // });
+  // test('ceasar cypher shift 1, test puncuation', () => {
+  //   expect(ceasarCipher('KITTEN', 1)).toBe('ljuufo')
+  // });
