c266c04fe786526da17ebe31fa33eed4de2fdb04
jared <114547220+goldjared@users.noreply.github.com>
2023-08-04 23:10:09 -0400

    create react app with vite, install prettier

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <div>
+      hello spyder
+    </div>
+    <h3>what the fuck</h3>
+
+    </>
+  );
+}
+
+export default App;
