c08d05b82e24c9bc6d125fa161f6ff12d112eedf
jared <114547220+goldjared@users.noreply.github.com>
2023-08-05 00:42:15 -0400

    modify main.jsx, create 2 new components

---
+++ my-first-react-app/src/assets/greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+  return <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>;
+}
+
+function Food() {
+  return <p>Pizza is what I want, pizza from scratch I shall try.</p>
+}
+
+export { Greeting, Food };
